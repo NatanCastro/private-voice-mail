91e8aaf800e1eb8ec7af5d39d5d9b9ab1e3f996a
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-09-20 20:08:48 -0300

    fix: fix tailwind font family sans

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,14 +7,14 @@ export default {
     extend: {
       fontFamily: {
         sans: [
-          'Inter"',
-          'ui-sans-serif',
-          'system-ui',
-          'sans-serif',
-          'Apple Color Emoji"',
-          'Segoe UI Emoji"',
-          'Segoe UI Symbol"',
-          'Noto Color Emoji"',
+          '"Inter"',
+          '"ui-sans-serif"',
+          '"system-ui"',
+          '"sans-serif"',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
         ],
       },
       borderRadius: {
