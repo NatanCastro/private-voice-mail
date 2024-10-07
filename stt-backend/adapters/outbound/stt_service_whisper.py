60932269d81dbe81bb0854ee82178401b84b613e
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-10-07 10:07:59 -0300

    feat: update whisper service to receive the audio bytes directly

--- stt-backend/adapters/outbound/stt_service_whisper.py
+++ stt-backend/adapters/outbound/stt_service_whisper.py
@@ -1,14 +1,13 @@
+import os
 from core.model.stt import SttResult
 from core.ports.request_service import IRequestService
-from core.ports.stt_service import SttService
 
 import torch
 from torch.nn.attention import SDPBackend, sdpa_kernel
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
 from tqdm import tqdm
 
-class WhisperSttService(SttService):
+class WhisperSttService:
     def __init__(self, request_service: IRequestService) -> None:
         self._request_service = request_service
         self._model = self._load_model()
@@ -26,7 +25,7 @@ class WhisperSttService(SttService):
         model.to(device)
 
         model.generation_config.cache_implementation = "static"
-        model.generation_config.max_new_tokens = 256
+        model.generation_config.max_new_tokens = 1024
         model.forward = torch.compile(model.forward, mode="reduce-overhead", fullgraph=True)
 
         processor = AutoProcessor.from_pretrained(model_id)
@@ -41,18 +40,16 @@ class WhisperSttService(SttService):
         )
         return pipe
 
-    def process_audio(self, url: str) -> SttResult:
-        print(f'processing {url}')
+    def process_audio(self, audio_data: bytes) -> str:
         result = None;
 
-        with open('audio.mp3', 'rb') as audio:
-            print(audio.read())
+
         for _ in tqdm(range(2), desc="Warm-up step"):
             with sdpa_kernel(SDPBackend.MATH):
-                result = self._model('audio.mp3', generate_kwargs={"language": "portuguese"})
-        for _ in range(4):
-            with sdpa_kernel(SDPBackend.MATH):
-                result = self._model('audio.mp3', generate_kwargs={"language": "portuguese"})
+                self._model(audio_data, batch_size=20*1024, generate_kwargs={"language": "portuguese"})
+
+        with sdpa_kernel(SDPBackend.MATH):
+            result = self._model(audio_data, generate_kwargs={"language": "portuguese"})
         __import__('pprint').pprint(result)
 
-        return SttResult('', '', '')
+        return result['text']
