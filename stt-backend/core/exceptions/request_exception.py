b19ae4fcfa3fa082e1907f74316cc3444c51f387
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-10-08 19:17:27 -0300

    feat: create custom exception for the requestservice

---
+++ stt-backend/core/exceptions/request_exception.py
@@ -0,0 +1,19 @@
+import enum
+
+
+class RequestErrorCode(enum.Enum):
+    INVALID_URL = 1
+    PROCESSING_RESPONSE = 2
+    EXTERNAL_ERROR = 3
+
+
+class RequestError(Exception):
+    _message: str
+    _code: RequestErrorCode
+
+    def __init__(self, message: str, code: RequestErrorCode) -> None:
+        self._message = message
+        self._code = code
+
+    def __str__(self) -> str:
+        return f"{self._message}, ERROR_CODE: {self._code}"
--- stt-backend/core/services/request_service.py
+++ stt-backend/core/services/request_service.py
@@ -1,28 +1,36 @@
+from loguru import logger
 import requests
+from requests.exceptions import InvalidSchema, InvalidURL, MissingSchema
 from result import Err, Ok, Result
 
+from core.exceptions.request_exception import RequestError, RequestErrorCode
 from core.ports.request_service import IRequestService, ResponseKind, T
 
 
 class RequestService(IRequestService):
     def __init__(self):
-        print("INFO: RequestService started")
+        logger.info("RequestService started")
 
-    def get(self, url: str, response_kind: ResponseKind) -> Result[T, str]:
-        response = None
+    def get(self, url: str, response_kind: ResponseKind) -> Result[T, RequestError]:
         try:
             response = requests.get(url)
-        except Exception as e:
-            return Err(f"invalid url: {e}")
 
-        match response_kind:
-            case ResponseKind.JSON:
-                try:
+            match response_kind:
+                case ResponseKind.JSON:
                     json_data = response.json()  # Expect dict
                     return Ok(json_data)  # Return Ok with dict type
-                except Exception as e:
-                    return Err(f"Could not transform response to JSON: {e}")
-            case ResponseKind.RAW:
-                return Ok(bytes(response.content))  # Return Ok with bytes type
-            case _:
-                raise ValueError("Unsupported ResponseKind")
+                case ResponseKind.RAW:
+                    return Ok(bytes(response.content))  # Return Ok with bytes type
+                case _:
+                    raise ValueError("Unsupported ResponseKind")
+
+        except Exception as e:
+            match e:
+                case InvalidURL() | InvalidSchema() | MissingSchema():
+                    return Err(RequestError(str(e), RequestErrorCode.INVALID_URL))
+                case requests.JSONDecodeError():
+                    return Err(
+                        RequestError(str(e), RequestErrorCode.PROCESSING_RESPONSE)
+                    )
+                case _:
+                    return Err(RequestError(str(e), RequestErrorCode.EXTERNAL_ERROR))
