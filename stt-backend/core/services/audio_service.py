f4177f178030273704d545b139b0f5a9398d9721
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-10-09 00:02:10 -0300

    feat: update logging on AudioService

--- stt-backend/core/services/audio_service.py
+++ stt-backend/core/services/audio_service.py
@@ -8,7 +8,7 @@ from core.ports.request_service import IRequestService, ResponseKind
 class AudioService(IAudioService):
     def __init__(self, request_service: IRequestService):
         self.request_service = request_service
-        print("INFO: AudioService server started")
+        logger.info("AudioService started")
 
     def get(self, url: str) -> Result[bytes, str]:
         result = self.request_service.get(url, ResponseKind.RAW)
