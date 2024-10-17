5432493995b8f5d0ddd01eff010b95310524dbb7
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-10-17 14:26:11 -0300

    feat: create function that fransforms a string into a stt_request

--- stt-backend/core/model/stt.py
+++ stt-backend/core/model/stt.py
@@ -1,12 +1,25 @@
 from dataclasses import dataclass
 from typing import Literal
 
+from result import Err, Ok, Result
+
 
 @dataclass
 class SttRequest:
     user_id: str
     audio_url: str
     language: str
+
+
+def stt_request_from_string(data: str) -> Result[SttRequest, str]:
+    split_data = list(filter(lambda i: i == "", data.split(",")))
+    if len(split_data) != 3:
+        return Err("invalid format")
+    (user_id, audio_url, language) = split_data
+    if not audio_url.startswith(tuple(["https://", "http://"])):
+        return Err("Invalid audio url")
+
+    return Ok(SttRequest(user_id, audio_url, language))
 
 
 @dataclass
